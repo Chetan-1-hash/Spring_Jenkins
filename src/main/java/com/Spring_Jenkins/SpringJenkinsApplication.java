2ed489c7bbe97cbb2bd57817dc1ac66061a4dff1
Chetan <chetan17302@gmail.com>
2024-03-31 18:23:48 +0530

    Initial commit

---
+++ src/main/java/com/Spring_Jenkins/SpringJenkinsApplication.java
@@ -0,0 +1,24 @@
+package com.Spring_Jenkins;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import jakarta.annotation.PostConstruct;
+
+@SpringBootApplication
+public class SpringJenkinsApplication {
+	
+	public static Logger logger = LoggerFactory.getLogger(SpringJenkinsApplication.class);
+	
+	@PostConstruct
+	public void display() {
+		logger.info("Application Started ....");
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJenkinsApplication.class, args);
+	}
+
+}
